e82826af0cdca0c1f4c25010e1753422a04c6a7c
Alex <contact.alexrobert04@gmail.com>
2022-09-12 01:12:11 +0200

    FEAT: Exit game

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,25 @@
+use game_engine::{Engine, Game, State, input::Key};
+
+struct GameOfLife {
+
+}
+
+impl GameOfLife {
+    pub fn new() -> Self {
+
+        GameOfLife {  }
+    }
+}
+
 fn main() {
+    let mut engine = Engine::new(30);
+    engine.run(|mut game: &mut Game, state: &mut State| {
+        for key in state.keyboard.get_last_key_press() {
+            match key {
+                Key::Q => state.exit = true,
+                Key::Unknown => ()
+            }
+        }
+        println!("Run Game");
+    });
 }
