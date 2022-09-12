91df6b3afbe7a80f3e51a09ad4ece68755cce5b0
Alex <contact.alexrobert04@gmail.com>
2022-09-12 13:15:30 +0200

    FEAT: Add Cell

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
-use game_engine::{Engine, Game, State, input::Key};
+use game_engine::{
+    input::Key,
+    render::{Pencil, Window},
+    spacial::Vector2D,
+    Engine, Game, State,
+};
+
+struct Cell {
+    pos: Vector2D,
+    life: Life,
+}
+
+enum Life {
+    Live,
+    Death,
+}
 
 struct GameOfLife {
-
+    cells: Vec<Cell>,
 }
 
 impl GameOfLife {
     pub fn new() -> Self {
-
-        GameOfLife {  }
+        GameOfLife { cells: Vec::new() }
     }
 }
 
 fn main() {
     let mut engine = Engine::new(30);
-    engine.run(|mut game: &mut Game, state: &mut State| {
-        for key in state.keyboard.get_last_key_press() {
-            match key {
-                Key::Q => state.exit = true,
-                Key::Unknown => ()
+    engine.run(
+        |mut game: &mut Game, state: &mut State, window: &mut Window| {
+            for key in state.keyboard.get_last_key_press() {
+                match key {
+                    Key::Q => state.exit = true,
+                    Key::Unknown => break,
+                }
             }
-        }
-        println!("Run Game");
-    });
+
+            let mut pencil = Pencil::new(window);
+            pencil.draw_text("Hello", Vector2D::new(10, 2));
+        },
+    );
 }
