f3ddf6ca96711518fd763bc6b28632041c19c3fd
Alex <contact.alexrobert04@gmail.com>
2022-09-12 16:36:54 +0200

    FEAT: Clic add cell, change vector cell to hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::time::Duration;
+use std::{time::Duration, collections::HashMap};
 
 use crossterm::event::{
     poll, read, Event, KeyCode, KeyEvent, KeyEventKind, KeyEventState, KeyModifiers, MouseButton,
@@ -7,65 +7,69 @@ use crossterm::event::{
 use game_engine::{
     render::{Pencil, Window},
     spacial::Vector2D,
-    Engine, Game, State,
+    Engine, State,
 };
 
-struct Cell {
-    pos: Vector2D,
-    life: Life,
-}
-
+#[derive(PartialEq, Eq)]
 enum Life {
     Live,
     Death,
 }
 
 struct GameOfLife {
-    cells: Vec<Cell>,
+    cells: HashMap<Vector2D, Life>,
 }
 
 impl GameOfLife {
     pub fn new() -> Self {
-        GameOfLife { cells: Vec::new() }
+        GameOfLife { cells: HashMap::new() }
     }
 }
 
 fn main() {
     let mut engine = Engine::new(30);
-    engine.run(
-        |mut game: &mut Game, state: &mut State, window: &mut Window| {
-            let mut pencil = Pencil::new(window);
+    let mut game = GameOfLife::new();
+    engine.run(|state: &mut State, window: &mut Window| {
+        let mut pencil = Pencil::new(window);
 
-            match poll(Duration::from_millis(0)) {
-                Ok(val) => {
-                    if val {
-                        match read().unwrap() {
-                            Event::Key(KeyEvent {
-                                code: KeyCode::Char('q'),
-                                modifiers: KeyModifiers::NONE,
-                                kind: KeyEventKind::Press,
-                                state: KeyEventState::NONE,
-                            })
-                            | Event::Key(KeyEvent {
-                                code: KeyCode::Char('Q'),
-                                modifiers: KeyModifiers::SHIFT,
-                                kind: KeyEventKind::Press,
-                                state: KeyEventState::NONE,
-                            }) => {
-                                state.exit = true;
-                            }
-                            Event::Mouse(event) => {
-                                if event.kind == MouseEventKind::Down(MouseButton::Left) {
-                                    pencil.draw_item('#', Vector2D::new(event.column as i32, event.row as i32));
-                                }
-                            }
-                            _ => (),
+        match poll(Duration::from_millis(0)) {
+            Ok(val) => {
+                if val {
+                    match read().unwrap() {
+                        Event::Key(KeyEvent {
+                            code: KeyCode::Char('q'),
+                            modifiers: KeyModifiers::NONE,
+                            kind: KeyEventKind::Press,
+                            state: KeyEventState::NONE,
+                        })
+                        | Event::Key(KeyEvent {
+                            code: KeyCode::Char('Q'),
+                            modifiers: KeyModifiers::SHIFT,
+                            kind: KeyEventKind::Press,
+                            state: KeyEventState::NONE,
+                        }) => {
+                            state.exit = true;
                         }
+                        Event::Mouse(event) => {
+                            if event.kind == MouseEventKind::Down(MouseButton::Left) {
+                                // game.cells.in(Cell {
+                                //     pos: Vector2D::new(event.column as i32, event.row as i32),
+                                //     life: Life::Live,
+                                // });
+                                game.cells.insert(Vector2D::new(event.column as i32, event.row as i32), Life::Live);
+                            }
+                        }
+                        _ => (),
                     }
                 }
-                Err(_) => (),
             }
-            pencil.draw_text("Hello", Vector2D::new(2, 2));
-        },
-    );
+            Err(_) => (),
+        }
+        // pencil.draw_text("Hello", Vector2D::new(2, 2));
+        for (cell, state_cell) in &game.cells {
+            if state_cell == &Life::Live {
+                pencil.draw_item('#', *cell);
+            }
+        }
+    });
 }
